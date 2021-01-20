653febf7c5e77ab3e63232221d4fbd51693cbff9
Ben Brandt <benjamin.j.brandt@gmail.com>
2021-01-19 20:43:07 -0800

    Add gnomes

--- src/character/race/gnome.rs
+++ src/character/race/gnome.rs
@@ -12,11 +12,14 @@ use crate::{
             in_inches, AgeRange, CharacteristicDetails, Characteristics, Gender,
             HeightAndWeightTable, Size, WeightMod,
         },
+        equipment::tools::{ArtisansTools, Tool},
         features::Feature,
+        languages::Language,
         names::{
             gnome::{CLAN, FEMALE, MALE, NICKNAMES},
             Name,
         },
+        proficiencies::Proficiency,
     },
     citation::{Book, Citation, Citations},
     dice_roller::{Die, RollCmd},
@@ -106,34 +109,95 @@ impl Race for Gnome {
         Citations(vec![race, subrace])
     }
 
+    fn languages(&self) -> Vec<Language> {
+        vec![Language::Common, Language::Gnomish]
+    }
+
     fn features(&self) -> Vec<Feature> {
-        let mut features = vec![Feature {
-            title: "Ability Score Increase",
-            description: "Your Intelligence score increases by 2.",
-            citation: Citation {
-                book: Book::PHB,
-                page: 36,
+        let mut features = vec![
+            Feature {
+                title: "Alignment",
+                description: "Gnomes are most often good. Those who tend toward law are sages, engineers, researchers, scholars, investigators, or inventors. Those who tend toward chaos are minstrels, tricksters, wanderers, or fanciful jewelers. Gnomes are good-hearted, and even the tricksters among them are more playful than vicious.",
+                citation: Citation {
+                    book: Book::PHB,
+                    page: 36,
+                },
             },
-        }];
+            Feature {
+                title: "Darkvision",
+                description: "Accustomed to life underground, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can't discern color in darkness, only shades of gray.",
+                citation: Citation {
+                    book: Book::PHB,
+                    page: 37,
+                },
+            },
+            Feature {
+                title: "Gnome Cunning",
+                description: "You have advantage on all Intelligence, Wisdom, and Charisma saving throws against magic.",
+                citation: Citation {
+                    book: Book::PHB,
+                    page: 37,
+                },
+            },
+        ];
         features.extend(match self.subrace {
-            GnomeSubrace::Forest => vec![Feature {
-                title: "Ability Score Increase",
-                description: "Your Dexterity score increases by 1.",
-                citation: Citation {
-                    book: Book::PHB,
-                    page: 37,
+            GnomeSubrace::Forest => vec![
+                Feature {
+                    title: "Natural Illusionist",
+                    description: "You know the minor illusion cantrip. Intelligence is your spellcasting ability for it.",
+                    citation: Citation {
+                        book: Book::PHB,
+                        page: 37,
+                    },
                 },
-            }],
-            GnomeSubrace::Rock => vec![Feature {
-                title: "Ability Score Increase",
-                description: "Your Constitution score increases by 1.",
-                citation: Citation {
-                    book: Book::PHB,
-                    page: 37,
+                Feature {
+                    title: "Speak with Small Beasts",
+                    description: "Through sounds and gestures, you can communicate simple ideas with Small or smaller beasts. Forest gnomes love animals and often keep squirrels, badgers, rabbits, moles, woodpeckers, and other creatures as beloved pets.",
+                    citation: Citation {
+                        book: Book::PHB,
+                        page: 37,
+                    },
                 },
-            }],
+            ],
+            GnomeSubrace::Rock => vec![
+                Feature {
+                    title: "Artificer's Lore",
+                    description: "Whenever you make an Intelligence (History) check related to magic items, alchemical objects, or technological devices, you can add twice your proficiency bonus, instead of any proficiency bonus you normally apply.",
+                    citation: Citation {
+                        book: Book::PHB,
+                        page: 37,
+                    },
+                },
+                Feature {
+                    title: "Artificer's Lore",
+                    description: "You have proficiency with artisan's tools (tinker's tools). Using those tools, you can spend 1 hour and 10 gp worth of materials to construct a Tiny clockwork device (AC 5, 1 hp). The device ceases to function after 24 hours (unless you spend 1 hour repairing it to keep the device functioning), or when you use your action to dismantle it; at that time, you can reclaim the materials used to create it. You can have up to three such devices active at a time.
+
+When you create a device, choose one of the following options:
+
+Clockwork Toy. This toy is a clockwork animal, monster, or person, such as a frog, mouse, bird, dragon, or soldier. When placed on the ground, the toy moves 5 feet across the ground on each of your turns in a random direction. It makes noises as appropriate to the creature it represents.
+
+Fire Starter. The device produces a miniature flame, which you can use to light a candle, torch, or campfire. Using the device requires your action.
+
+Music Box. When opened, this music box plays a single song at a moderate volume.
+The box stops playing when it reaches the song's end or when it is closed.",
+                    citation: Citation {
+                        book: Book::PHB,
+                        page: 37,
+                    },
+                },
+            ],
         });
         features
+    }
+
+    fn proficiencies(&self) -> Vec<Proficiency> {
+        let mut proficiencies = vec![];
+        if let GnomeSubrace::Rock = self.subrace {
+            proficiencies.push(Proficiency::Tool(Tool::ArtisansTools(
+                ArtisansTools::TinkersTools,
+            )))
+        }
+        proficiencies
     }
 }
 
