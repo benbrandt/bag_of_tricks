3d90c873e18ed7f1bf8096a8bf819331ffc8c6eb
Ben Brandt <benjamin.j.brandt@gmail.com>
2021-05-05 14:11:33 -0700

    Back to tide

--- bag_of_tricks/src/main.rs
+++ bag_of_tricks/src/main.rs
@@ -3,104 +3,41 @@
 
 use std::env;
 
-use actix_http::{body::Body, http::StatusCode, Response};
-use actix_web::{
-    dev::ServiceResponse,
-    error::ErrorInternalServerError,
-    get,
-    middleware::{
-        errhandlers::{ErrorHandlerResponse, ErrorHandlers},
-        Compress, Logger,
-    },
-    web::{scope, Data},
-    App, Error, HttpResponse, HttpServer, Result,
-};
-use env_logger::Env;
+use character::Character;
 use rand::SeedableRng;
 use rand_pcg::Pcg64;
-use tera::{Context, Tera};
+use tera::Tera;
+use tide_compress::CompressMiddleware;
+use tide_tera::{context, TideTeraExt};
 
-use character::Character;
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    tide::log::start();
 
-#[get("/")]
-async fn index(tmpl: Data<Tera>) -> Result<HttpResponse, Error> {
-    let mut rng = Pcg64::from_entropy();
-    let mut ctx = Context::new();
-    ctx.insert("character", &format!("{}", Character::gen(&mut rng)));
-    let s = tmpl
-        .render("index.html", &ctx)
-        .map_err(|_| ErrorInternalServerError("Template error"))?;
-    Ok(HttpResponse::Ok().content_type("text/html").body(s))
-}
-
-// Custom error handlers, to return HTML responses when an error occurs.
-fn error_handlers() -> ErrorHandlers<Body> {
-    ErrorHandlers::new().handler(StatusCode::NOT_FOUND, |res| {
-        let response = get_error_response(&res, "Page not found");
-        Ok(ErrorHandlerResponse::Response(
-            res.into_response(response.into_body()),
-        ))
-    })
-}
-
-// Generic error handler.
-fn get_error_response<B>(res: &ServiceResponse<B>, error: &str) -> Response<Body> {
-    let request = res.request();
-
-    // Provide a fallback to a simple plain text response in case an error occurs during the
-    // rendering of the error page.
-    let fallback = |e: &str| {
-        Response::build(res.status())
-            .content_type("text/plain")
-            .body(e.to_string())
-    };
-
-    let tera = request.app_data::<Data<Tera>>().map(Data::get_ref);
-    match tera {
-        Some(tera) => {
-            let mut ctx = Context::new();
-            ctx.insert("error", error);
-            ctx.insert("status_code", res.status().as_str());
-            let body = tera.render("error.html", &ctx);
-
-            match body {
-                Ok(body) => Response::build(res.status())
-                    .content_type("text/html")
-                    .body(body),
-                Err(_) => fallback(error),
-            }
-        }
-        None => fallback(error),
-    }
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    env::set_var("RUST_BACKTRACE", "1");
     let _guard = sentry::init((
         "https://ffdf2fc8b5ff48c4a1e5240f9679abd7@o251876.ingest.sentry.io/5628414",
         sentry::ClientOptions::default(),
     ));
-    env::set_var("RUST_BACKTRACE", "1");
 
-    // Get the port number to listen on.
-    let port = env::var("PORT")
+    let port: String = env::var("PORT")
         .unwrap_or_else(|_| "3000".to_string())
         .parse()
         .expect("PORT must be a number");
 
-    HttpServer::new(|| {
-        let tera = Tera::new("bag_of_tricks/templates/**/*").unwrap();
+    let mut tera = Tera::new("bag_of_tricks/templates/**/*")?;
+    tera.autoescape_on(vec!["html"]);
 
-        App::new()
-            .data(tera)
-            .wrap(sentry_actix::Sentry::new())
-            .wrap(Logger::default())
-            .wrap(Compress::default())
-            .service(index)
-            .service(scope("").wrap(error_handlers()))
-    })
-    .bind(("0.0.0.0", port))?
-    .run()
-    .await
+    let mut app = tide::with_state(tera);
+    app.with(CompressMiddleware::new());
+    app.at("/").get(|req: tide::Request<Tera>| async move {
+        let mut rng = Pcg64::from_entropy();
+        let tera = req.state();
+        tera.render_response(
+            "index.html",
+            &context! { "character" => format!("{}", Character::gen(&mut rng)) },
+        )
+    });
+    app.listen(format!("127.0.0.1:{}", port)).await?;
+    Ok(())
 }
